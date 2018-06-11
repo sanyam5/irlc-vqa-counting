50cb80bc40aba435b31e1c0b9c4dfef89301066a
Sanyam Agarwal <sanyam.agarwal94@gmail.com>
2018-06-11 21:14:24 +0530

    Coded up IRLC trying to stick as close as possible to the specifications. But unable to train. Not sure if due to hyper-params or some bug in calculating the loss

--- model.py
+++ model.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 from config import *
 from torch.nn.utils.weight_norm import weight_norm
 import torch.nn.functional as F
+from torch.autograd import Variable
 
 
 class QuestionParser(nn.Module):
@@ -104,3 +105,278 @@ class SoftCount(nn.Module):
         soft_counts = F.sigmoid(self.W(s)).squeeze(2)  # (B, k)
         C = soft_counts.sum(dim=1)  # (B,)
         return C
+
+
+class RhoScorer(nn.Module):
+
+    def __init__(self):
+        super(RhoScorer, self).__init__()
+        self.W = weight_norm(nn.Linear(QuestionParser.ques_dim, 1), dim=None)
+
+        inp_dim = 1 + 1 + 6 + 6 + 1 + 1 + 1  # 17
+        self.f_rho = FCNet([inp_dim, 100, 1])
+
+    @staticmethod
+    def get_spatials(b):
+        # b = (B, k, 6)
+
+        b = b.float()
+
+        B, k, _ = b.size()
+
+        b_ij = torch.stack([b] * k, dim=1)  # (B, k, k, 6)
+        b_ji = b_ij.transpose(1, 2)
+
+        area_ij = (b_ij[:, :, :, 2] - b_ij[:, :, :, 0]) * (b_ij[:, :, :, 3] - b_ij[:, :, :, 1])
+        area_ji = (b_ji[:, :, :, 2] - b_ji[:, :, :, 0]) * (b_ji[:, :, :, 3] - b_ji[:, :, :, 1])
+
+        righmost_left = torch.max(b_ij[:, :, :, 0], b_ji[:, :, :, 0])
+        downmost_top = torch.max(b_ij[:, :, :, 1], b_ji[:, :, :, 1])
+        leftmost_right = torch.min(b_ij[:, :, :, 2], b_ji[:, :, :, 2])
+        topmost_down = torch.min(b_ij[:, :, :, 3], b_ji[:, :, :, 3])
+
+        # calucate the separations
+        left_right = (leftmost_right - righmost_left)
+        up_down = (topmost_down - downmost_top)
+
+        # don't multiply negative separations,
+        # might actually give a postive area that doesn't exit!
+        left_right = torch.max(0*left_right, left_right)
+        up_down = torch.max(0*up_down, up_down)
+
+        overlap = left_right * up_down
+
+        iou = overlap / (area_ij + area_ji - overlap)
+        o_ij = overlap / area_ij
+        o_ji = overlap / area_ji
+
+        iou = iou.unsqueeze(3)  # (B, k, k, 1)
+        o_ij = o_ij.unsqueeze(3)  # (B, k, k, 1)
+        o_ji = o_ji.unsqueeze(3)  # (B, k, k, 1)
+
+        return b_ij, b_ji, iou, o_ij, o_ji
+
+    def forward(self, q_emb, v_emb, b):
+        # q_emb = (B, ques_size)
+        # v_emb = (B, k, v_dim)
+        # b = (B, k, 6)
+
+        B, k, _ = v_emb.size()
+
+        features = []
+
+        wq = self.W(q_emb).squeeze(1)  # (B,)
+        wq = wq[:, None, None, None].repeat(1, k, k, 1)  # (B, k, k, 1)
+        assert wq.size() == (B, k, k, 1), "wq size is {}".format(wq.size())
+        features.append(wq)
+
+        norm_v_emb = F.normalize(v_emb, dim=2)  # (B, k, v_dim)
+        vtv = torch.bmm(norm_v_emb, norm_v_emb.transpose(1, 2))  # (B, k, k)
+        vtv = vtv[:, :, :, None].repeat(1, 1, 1, 1)  # (B, k, k, 1)
+        assert vtv.size() == (B, k, k, 1)
+        features.append(vtv)
+
+        b_ij, b_ji, iou, o_ij, o_ji = self.get_spatials(b)
+
+        assert b_ij.size() == (B, k, k, 6)
+        assert b_ji.size() == (B, k, k, 6)
+        assert iou.size() == (B, k, k, 1)
+        assert o_ij.size() == (B, k, k, 1)
+        assert o_ji.size() == (B, k, k, 1)
+
+        features.append(b_ij)  # (B, k, k, 6)
+        features.append(b_ji)  # (B, k, k, 6)
+        features.append(iou)  # (B, k, k, 1)
+        features.append(o_ij)  # (B, k, k, 1)
+        features.append(o_ji)  # (B, k, k, 1)
+
+        features = torch.cat(features, dim=3)  # (B, k, k, 17)
+
+        rho = self.f_rho(features).squeeze(3)  # (B, k, k)
+
+        return rho, features  # (B, k, k)
+
+
+class IRLC(nn.Module):
+
+    def __init__(self):
+        super(IRLC, self).__init__()
+        self.ques_parser = QuestionParser()
+        self.f_s = ScoringFunction()
+        self.W = weight_norm(nn.Linear(self.f_s.score_dim, 1), dim=None)
+        self.f_rho = RhoScorer()
+
+        # extra custom parameters
+        self.eps = nn.Parameter(torch.zeros(1))
+        self.extra_params = nn.ParameterList([self.eps])
+
+    def sample_action(self, weights, already_selected=None, greedy=False):
+        # weights = (B, num classes)
+        # already_selected = (num_timesteps, B)
+
+        if already_selected is None:
+            mask = 1
+        else:
+            mask = Variable(torch.ones(weights.size()))
+            if USE_CUDA:
+                # TODO: uncomment this, when this model works
+                mask = mask.cuda()
+                pass
+            mask = mask.scatter_(1, already_selected.t(), 0)
+
+        masked_weights = mask * (weights + 1e-12)  # (B, k+1), add epsilon to make sure no non-masked value is zero.
+
+        if greedy:
+            _, a = masked_weights.max(dim=1)  # (B)
+        else:
+            a = masked_weights.multinomial(num_samples=1).squeeze(dim=1)  # (B)
+
+        return a
+
+    @staticmethod
+    def get_interaction(rho, a):
+        # get the interaction row in rho corresponding to the action a
+        # rho = (B, num_actions, k)
+        # a = (B) containing action indices between 0 and num_actions-1
+
+        B, _, k = rho.size()
+
+        # first expand a to the size required output
+        a = a[:, None].repeat(1, k)  # (B, k)
+
+        # print("rho size = {} and a size = {}".format(rho.size(), a.size()))
+        interaction = rho.gather(dim=1, index=a.unsqueeze(dim=1)).squeeze(dim=1)  # (B, k)
+        assert interaction.size() == (B, k), "interaction size is {}".format(interaction.size())
+        # print("interaction size = {}".format(interaction.size()))
+
+        return interaction  # (B, k)
+
+    def sample_objects(self, kappa_0, rho, batch_eps, greedy=False):
+        # kappa_0 = (B, k)
+        # rho = (B, k, k)
+
+        # add an extra row of 0 interaction for the terminal action
+        rho = torch.cat((rho, 0 * rho[:, :1, :]), dim=1)  # (B, k+1, k)
+
+        B, k = kappa_0.size()
+
+        P = None  # will store probability values at each timestep.
+        A = None  # will store action values at each timestep.
+        T = k+1  # num timesteps = different possible actions. +1 for the terminal action
+        kappa = kappa_0  # (B, k), starting kappa
+
+        for t in range(T):
+            # calculate probabilities of each action
+            p = F.softmax(torch.cat((kappa, batch_eps), dim=1), dim=1)  # (B, k+1)
+            # print("p = ", p)
+            # select one object (called "action" in RL terms), avoid already selected objects.
+            a = self.sample_action(weights=p, already_selected=A, greedy=greedy)  # (B)
+            # update kappa logits with the row in the interaction matrix corresponding to the chosen action.
+            interaction = self.get_interaction(rho, a)
+            kappa = kappa + interaction
+
+            # record the prob and action values at each timestep for later use
+            P = p[None] if P is None else torch.cat((P, p[None]), dim=0)  # (t+1, B, k+1)
+            A = a[None] if A is None else torch.cat((A, a[None]), dim=0)  # (t+1, B)
+
+        assert P.size() == (T, B, k+1)
+        assert A.size() == (T, B)
+
+        # calculate count
+        terminal_action = (A == k)  # (T, B)  # true for the timestep when terminal action was selected.
+        _, count = terminal_action.max(dim=0)  # (B,)  # index of the terminal action is considered the count
+
+        return P, A, count
+
+    def compute_vars(self, v_emb, b, q):
+        # v_emb = (B, k, v_dim)
+        # b = (B, k, 6)
+        # q = (B, MAXLEN)
+
+        B, k, _ = v_emb.size()
+
+        q_emb = self.ques_parser(q)  # (B, q_dim)
+        s = self.f_s(v_emb, q_emb)  # (B, k, score_dim)
+        kappa_0 = self.W(s).squeeze(2)  # (B, k)
+
+        rho, _ = self.f_rho(q_emb, v_emb, b)  # (B, k, k)
+        batch_eps = torch.cat([self.eps] * B)[:, None]  # (B, 1)
+        return kappa_0 , rho, batch_eps
+
+    def take_mc_samples(self, kappa_0, rho, batch_eps, num_mc_samples):
+
+        P, A, count = self.sample_objects(kappa_0=kappa_0, rho=rho, batch_eps=batch_eps)
+        _, _, greedy_count = self.sample_objects(kappa_0=kappa_0, rho=rho, batch_eps=batch_eps, greedy=True)
+
+        # we will concatenate any additional MC samples along the "batch" dimension since that is eventually averaged.
+        for _ in range(num_mc_samples - 1):
+            another_P, another_A, another_count = self.sample_objects(kappa_0=kappa_0, rho=rho, batch_eps=batch_eps)
+            _, _, another_greedy_count = self.sample_objects(kappa_0=kappa_0, rho=rho, batch_eps=batch_eps, greedy=True)
+
+            P = torch.cat((P, another_P), dim=1)  # (T, B*samples, k+1)
+            A = torch.cat((A, another_A), dim=1)  # (T, B*samples)
+            count = torch.cat((count, another_count), dim=0)  # (B*samples)
+            greedy_count = torch.cat((greedy_count, another_greedy_count), dim=0)  # (B*samples)
+
+        return count, greedy_count, P, A, rho
+
+    # def forward(self, v_emb, b, q, num_mc_samples=1):
+    #     # v_emb = (B, k, v_dim)
+    #     # b = (B, k, 6)
+    #     # q = (B, MAXLEN)
+    #
+    #     kappa_0, rho, batch_eps = self.compute_vars(v_emb, b, q)
+    #     count, greedy_count, P, A, rho = self.take_mc_samples(kappa_0, rho, batch_eps, num_mc_samples)
+    #
+    #     return count, greedy_count, P, A, rho
+
+    def get_loss(self, count_gt, count, greedy_count, P, A, rho):
+        # count_gt = (B,)
+        # count = (B,)
+        # greedy_count = (B,)
+        # P = (T, B, k+1)
+        # A = (T, B)
+        # rho = (B, k, k)
+
+        assert count.size() == count_gt.size()
+        assert greedy_count.size() == count_gt.size()
+
+        count = count.float()
+        greedy_count = greedy_count.float()
+        count_gt = count_gt.float()
+
+        # self-critical loss
+        E = torch.abs(count - count_gt)  # (B,)
+        E_greedy = torch.abs(greedy_count - count_gt)  # (B,)
+        R = E_greedy - E  # (B,)
+        assert R.size() == count.size(), "R size is {}".format(R.size())
+
+        # we need to select the probabilities of selection
+        PA = P.gather(dim=2, index=A.unsqueeze(2)).squeeze(dim=2)  # (T, B)
+        assert PA.size() == A.size()
+
+        log_PA = torch.log(PA)  # (T, B)
+        sum_log_PA = log_PA.sum(dim=0)  # (B,)
+        batch_sc_loss = - R * sum_log_PA  # (B,)
+        sc_loss = batch_sc_loss.mean(dim=0)  # (1,)
+
+        # P entropy loss
+        def H(probs, dim):
+            mults = probs * torch.log(probs)
+            return - mults.sum(dim=dim)
+
+        batch_entropy_loss = H(P, dim=2).sum(dim=0)  # (B,)
+        entropy_loss = batch_entropy_loss.mean(dim=0)
+
+        # interaction strength, lower is better, sparse preferred.
+        interaction_strengths = F.smooth_l1_loss(rho, 0*rho.detach(), reduce=False)  # (B, k, k)
+        interaction_strengths = interaction_strengths.mean(dim=2)  # (B, k)
+        # TODO: not sure if to be done only for selected actions. doing for all now.
+        # selected_interactions = interactions[A]  # (T, B)
+        # # sum along the time dimension
+        batch_interaction_strength = interaction_strengths.sum(dim=1)  # (B,)
+        interaction_strength = batch_interaction_strength.mean(dim=0)
+
+        loss = 1.0 * sc_loss # + .005 * entropy_loss + .005 * interaction_strength
+
+        return loss
